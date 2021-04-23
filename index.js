1e00a0967f701f24f9466fc18751a49300af0cf1
CeyhunGulbas <cetree61@gmail.com>
2021-04-23 21:18:28 +0300

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 // Your code here
+/*
 let dodger = document.getElementById("dodger");
 
 function moveDodgerLeft() {
@@ -30,3 +31,36 @@ document.addEventListener("keydown", function(e) {
     moveDodgerRight();
 }
 }); 
+*/
+
+let dodger = document.getElementById("dodger");
+
+function moveDodgerLeft() {
+    let leftNumbers = dodger.style.left.replace("px", "");
+    let left = parseInt(leftNumbers, 10);
+   
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
+    }
+  }
+
+  document.addEventListener("keydown", function(e) {
+    if (e.key === "ArrowLeft") {
+      moveDodgerLeft();
+    }
+  });
+
+  function moveDodgerRight() {
+    let leftNumbers = dodger.style.left.replace("px", "");
+    let left = parseInt(leftNumbers, 10);
+   
+    if (left > 0) {
+        dodger.style.left = `${left + 1}px`;
+    }
+  }
+
+  document.addEventListener("keydown", function(e) {
+    if (e.key === "ArrowRight") {
+      moveDodgerRight();
+    }
+  });
