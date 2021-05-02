7afb76577a487dcaef1ca03f157826151b855d96
Ali Rıza <ars.lifestyle@hotmail.com>
2021-05-02 12:21:58 +0300

    Done.

--- index.js
+++ index.js
@@ -26,10 +26,9 @@ function moveDodgerLeft(e) {
 let leftNumbers = dodger.style.left.replace("px", "");
 let left = parseInt(leftNumbers, 10);
 
-    if (left >= 0) {
+    if (left > 0) {
       
         dodger.style.left = left - 1 + "px";
-        // dodger.style.right = (right + 1) + "px";
     }
 
 }
@@ -39,24 +38,8 @@ function moveDodgerRight(e) {
 let leftNumbers = dodger.style.left.replace("px", "");
 let left = parseInt(leftNumbers, 10);
         
-        if (left <= 360) {
-    
+        if (left < 360) {
     
             dodger.style.left = left + 1 + "px";
-            // dodger.style.left = (left + 1) + "px";
         }
     }
-
-// function moveDodgerRight(e) {
-
-// // dodger.style.left = "";
-// let rightNumbers = dodger.style.right.replace("px", "");
-// let right = parseInt(rightNumbers, 10);
-    
-//     if (right >= 0) {
-
-
-//         dodger.style.right = right - 1 + "px";
-//         // dodger.style.left = (left + 1) + "px";
-//     }
-// }
